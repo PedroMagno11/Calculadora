422da4137d8a1303fbb8257c551aefe6c3a709b4
Pedro Magno <pedromagnopro@gmail.com>
2023-01-08 11:27:22 -0300

    Projeto concluído com todas a funcionalidades

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const input = document.getElementById("input")
 const resultInput = document.getElementById("result")
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
-document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
-  charKeyBtn.addEventListener("click", function () {
-    const value = charKeyBtn.dataset.value
-    input.value += value
+document.querySelectorAll(".charKey").forEach(function(charKeyBtn){
+  charKeyBtn.addEventListener("click",()=>{
+    const value = charKeyBtn.dataset.value;
+    input.value+= value;
   })
 })
 
